68f3bd8c70d4b97a7a076936851733f2ea6198ee
LukeAFullard <LukeAFullard@gmail.com>
2025-06-27 20:25:28 +1200

    create folder initiation/prep function

--- script2stlite/script2stlite.py
+++ script2stlite/script2stlite.py
@@ -1,13 +1,29 @@
-from functions import load_all_versions
+from functions import load_all_versions,folder_exists,get_current_directory,create_directory,copy_file_from_subfolder,file_exists
+import os
 
-def s2s_prepare_folder():
-    #destination 
-    pass
+def s2s_prepare_folder(directory = None):
+    #1. check if user provided a directory (or we will use current dir)
+    if directory is not None: #directory is provided
+        #check provided directory is valid
+        if not folder_exists(directory): raise ValueError(f'''* {directory} does not exist on this system.''')
+    else:  #nodirectory provided, use cd
+        directory = get_current_directory()  
+        print(f"* No user directory provided. Creating new s2stlite project in current directory ({directory}). \n")
+    #2. check if pages folder exists, if not, create it
+    create_directory(os.path.join(directory,'pages'))
+    #3. create settings fileif it doesn't exist.
+    if not file_exists(os.path.join(directory,'settings.yaml')):
+        if not copy_file_from_subfolder(subfolder='templates',filename='settings.yaml',destination_dir=directory):
+            raise ValueError(f'''* Issue copying settings template.''')
+    else: print(f"* settings.yaml already exists in {directory}. NO NEW FILE CREATED. \n")        
+    print(f"* Folder structure successfully created: {directory}. \n")
+
+        
 
 
 
 def s2s_convert(stlite_version = None, pyodide_version = None):
-    #load settings
+    #1. load settings
     stylesheet_versions, stylesheet_top_version, js_versions, js_top_version, pyodide_versions, pyodide_top_version = load_all_versions()
     
     if stlite_version is None:
